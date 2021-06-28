46b16e7867e43079e5c44cd742b07c68efdc18ec
Alejandro Molina <alemosan@hotmail.com>
2021-06-28 13:06:40 +0200

    Sonarqube added

---
+++ pac/sonarqube.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent {
+        node {
+            label 'evol5-sonarqube'
+        }
+    }
+    options {
+        timeout(time: 30, unit: 'MINUTES')
+    }
+    environment {
+        PATH = "/usr/local/bin:/usr/local/bin:$PATH"
+        ARTIFACTORY_CRED=credentials('artifactory_credentials')
+        SCANNERHOME = tool 'Sonar Scanner 4';
+    }
+    stages {
+
+        stage('SonarQube analysis') {
+            steps {
+                    sh '''
+
+                        ${SCANNERHOME}/bin/sonar-scanner \
+                            -Dsonar.projectKey=Evolved5g \
+                            -Dsonar.projectBaseDir=/home/ams@hi.inet/redes_uds \
+                            -Dsonar.host.url=http://10.95.133.49 \
+                            -Dsonar.login=74d6707285b5c1c79f99b71ca4337e6fd5c31cd8
+                    '''
+                }
+            }
+        }
+    }
