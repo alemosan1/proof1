d789dfbb9aab02b58047102c4e933845e9dcc35d
Alejandro Molina <alemosan@hotmail.com>
2021-06-28 16:10:37 +0200

    hola234233333

--- pac/sonarqube.groovy
+++ pac/sonarqube.groovy
@@ -19,6 +19,7 @@ pipeline {
                         sudo ${SCANNERHOME}/bin/sonar-scanner -X \
                             -Dsonar.projectKey=Evolved5g \
                             -Dsonar.projectBaseDir=/home/ams@hi.inet/redes_uds \
+                            -Dsonar.sources=. \
                             -Dsonar.host.url=http://10.95.133.49 \
                             -Dsonar.login=8785bc53c0479224e256beeff2f610e5aa157425
                     '''
