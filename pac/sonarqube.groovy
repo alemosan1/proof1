98dd9213c96f50a3da7aa5c983f3e92365b5ed65
Alejandro Molina <alemosan@hotmail.com>
2021-06-29 09:11:38 +0200

    Adding UTF-8 encoding 1

--- pac/sonarqube.groovy
+++ pac/sonarqube.groovy
@@ -17,7 +17,7 @@ pipeline {
             steps {
                     sh '''
                         sudo ${SCANNERHOME}/bin/sonar-scanner -X \
-                            -Dsonar.projectKey=Evolved5g \
+                            -Dsonar.projectKey=Evolved5g-master\
                             -Dsonar.projectBaseDir=/home/ams@hi.inet/sonar-java/ \
                             -Dsonar.host.url=http://10.95.133.49 \
                             -Dsonar.login=40f1332530d31e2372160616f6a458b82c5e429d \
