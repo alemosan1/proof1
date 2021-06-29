ebb219b1afe3913c85af11ae07349cc629079ae5
Alejandro Molina <alemosan@hotmail.com>
2021-06-29 09:16:15 +0200

    Python project

--- pac/sonarqube.groovy
+++ pac/sonarqube.groovy
@@ -18,13 +18,12 @@ pipeline {
                     sh '''
                         sudo ${SCANNERHOME}/bin/sonar-scanner -X \
                             -Dsonar.projectKey=Evolved5g-master\
-                            -Dsonar.projectBaseDir=/home/ams@hi.inet/sonar-java/ \
+                            -Dsonar.projectBaseDir=/home/ams@hi.inet//sonar-scanning-examples/sonarqube-scanner/src/python/ \
                             -Dsonar.host.url=http://10.95.133.49 \
                             -Dsonar.login=40f1332530d31e2372160616f6a458b82c5e429d \
                             -Dsonar.projectName=Evolved5g-master \
-                            -Dsonar.language=java \
+                            -Dsonar.language=python \
                             -Dsonar.sourceEncoding=UTF-8 \
-                            -Dsonar.java.binaries=**/target/classes  
                     '''
                 }
             }
